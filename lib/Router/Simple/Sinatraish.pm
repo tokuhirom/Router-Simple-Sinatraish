9100320efa720a5c6fc8ea9c483c13b2bcf0d8d9
tokuhirom <tokuhirom@gmail.com>
2010-07-22 10:32:09 +0900

    initial import

---
+++ lib/Router/Simple/Sinatraish.pm
@@ -0,0 +1,39 @@
+package Router::Simple::Sinatraish;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Router::Simple::Sinatraish -
+
+=head1 SYNOPSIS
+
+  use Router::Simple::Sinatraish;
+
+=head1 DESCRIPTION
+
+Router::Simple::Sinatraish is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
